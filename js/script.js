b39e9d2ec8ca882d225db002e18a194141362bcf
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-19 10:33:19 -0700

    add html, css and js files to project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+
+function subtract(number3, number4) {
+  return number3 + number4;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+
+function multiply(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+
+function divide(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);
