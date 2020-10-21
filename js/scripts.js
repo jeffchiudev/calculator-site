87f84dec262c3792aca158628bcc1589831dd64c
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-21 16:31:43 -0700

    add radio options and secondary input fields
    co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,22 @@ const divide = function(number1, number2) {
   return number1 / number2;
 };
 
+const flavor = $("input:radio[name=flavor]:checked").val();
+
+
 $(document).ready(function() {
-  // $("form#calculate").submit(function(event) {
-  //   event.preventDefault();
-  //   const number1 = parseInt($("#num1").val());
-  //   const number2 = parseInt($("#num2").val());
-  //   let result = add(number1, number2);
-  //   $("#output").text(result);
-   $("form#calculate").submit(function(event) {
-     event.preventDefault();
-     const number1 = parseInt($("#num1").val());
-     const number2 = parseInt($("#num2").val());
-     let result = subtract(number1, number2);
+  $("form#calculate").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    let result = add(number1, number2);
+    $("#output").text(result);
+  $("form#calculate").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    let result = subtract(number1, number2);
     $("#output").text(result);
   });
-  // });
+  });
 });
