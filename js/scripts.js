73934631e6630b210a819ddbcd613ca5c3ad9dd6
Jeff Chiu <jwchiu82@gmail.com>
2020-10-22 14:38:14 -0700

    add function functionality depending on radio button
    co-authored-by: Patrick Osten <patrickosten040@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,22 +14,25 @@ const divide = function(number1, number2) {
   return number1 / number2;
 };
 
-const flavor = $("input:radio[name=flavor]:checked").val();
+
 
 
 $(document).ready(function() {
-  $("form#calculate").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#num1").val());
-    const number2 = parseInt($("#num2").val());
-    let result = add(number1, number2);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
-  $("form#calculate").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#num1").val());
-    const number2 = parseInt($("#num2").val());
-    let result = subtract(number1, number2);
-    $("#output").text(result);
-  });
   });
 });
